daf4e4ef105ab71d306153f437f192ae5ef3ba31
M-Danish-Meraj <muhammaddanishmeraj@gmail.com>
2024-10-24 00:21:33 +0500

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
-int main()
-{
-      printf(" A B C D E F G H\n");
-    for(int row=0;row<8;row++)
-    {
-        printf("%d ",row+1);
-            for (int  col = 0; col < 8; col++)
-                {
-            if(row <= 2 && (row + col) % 2 == 1)
-                printf("\033[31mR\033[0m"); 
-            else if(row >= 5 && (row + col) % 2 == 1)
-                printf("\033[34mB\033[0m");
-            else {
-                printf("  ");}
-                }
-         printf("\n");
+
+int main() {
+    char board[8][8] = {
+        {' ', 'B', ' ', 'B', ' ', 'B', ' ', 'B'},
+        {'B', ' ', 'B', ' ', 'B', ' ', 'B', ' '},
+        {' ', 'B', ' ', 'B', ' ', 'B', ' ', 'B'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'R', ' ', 'R', ' ', 'R', ' ', 'R', ' '},
+        {' ', 'R', ' ', 'R', ' ', 'R', ' ', 'R'},
+        {'R', ' ', 'R', ' ', 'R', ' ', 'R', ' '}
+    };
+
+    // Print column indices
+    printf("  ");
+    for (int i = 0; i < 8; i++) {
+        printf("%d ", i);  // Print column numbers
     }
+    printf("\n");
+
+    // Print the board with row indices
+    for (int i = 0; i < 8; i++) {
+        printf("%d ", i);  // Print row number
+        for (int j = 0; j < 8; j++) {
+           if (board[i][j]=='B')
+           {
+            printf("\033[34m%c\033[0m ", board[i][j]);
+           }else if (board[i][j]=='R'){
+            printf("\033[31m%c\033[0m ", board[i][j]);
+           }
+           else
+            printf("%c ", board[i][j]);  // Print board element with a space
+       
+        }
+        printf("\n");  // Move to the next row
+    }
+
+
     return 0;
 }
+
 
